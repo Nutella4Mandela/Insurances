af70c262b53a066b0acbfd35653b83dc048a4fd1
Nelson Villegas <98365702+Nutella4Mandela@users.noreply.github.com>
2025-02-09 20:12:35 -0500

    Add files via upload

---
+++ Insurances/InsuranceDefine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InsuranceDefine
+{
+    public string insuranceName { get; set; }
+    public int isPartnered { get; set; }
+    public int needReferral { get; set; }
+    public int hasPlans { get; set; }
+    public string otherRequire { get; set; }
+
+    public override string ToString()
+    {
+        return insuranceName;
+    }
+
+
+}
+
---
+++ Insurances/InsuranceDefiner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+public class InsuranceDefiner
+{
+    [XmlAttribute(AttributeName = "Name")]
+    public string InsName { get; set; }
+    
+    [XmlAttribute(AttributeName = "Partnered")]
+    public int Partnered { get; set; }
+    
+    [XmlAttribute(AttributeName = "Referral")]
+    public int Referral { get; set; }
+    
+    [XmlAttribute(AttributeName = "Plans")]
+    public int Plans { get; set; }
+    
+    [XmlAttribute(AttributeName = "Requirements")]
+    public string Requirements { get; set; }
+}
+
